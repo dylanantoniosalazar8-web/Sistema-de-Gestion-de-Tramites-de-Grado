d6f19e02bc94843a575d85f49eb431c3a1cf0564
Dylansala <dylanantoniosalazar8@gmail.com>
2026-03-10 04:21:36 -0500

    Delete app directory

--- app/models/paz_ysalvo_model.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class PazYSalvo(BaseModel):
-    id_pazsalvo: Optional[int] = None
-    id_estudiante: int
-    biblioteca: bool
-    financiero: bool
-    laboratorio: bool
--- app/models/programas_model.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class Programa(BaseModel):
-    id_programa: Optional[int] = None
-    nombre: str
-    facultad_id: Optional[int]
